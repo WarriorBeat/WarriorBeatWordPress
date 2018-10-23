2c1c736e470d33383d1af9743a20436d938aa536
Braden Mars <bradenmars02@gmail.com>
2018-10-22 21:45:38 -0500

    Dockerization, Docker Compose & Bootstrap
    * Dockerized
    * Docker bootstrap:
     - Installs wp cli
     - Configures WordPress
     - Installs notification and blackbar plugin

---
+++ docker-bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# WordPress CLI
+echo "Installing WP-CLI"
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+# WP Config
+echo "Setting WP Config Options"
+wp config set WP_HOME http://localhost:3000 --type=constant --allow-root
+wp config set WP_SITEURL http://localhost:3000 --type=constant --allow-root
+wp config set WP_DEBUG true --raw --type=constant --allow-root
+wp config set WP_DEBUG_DISPLAY false --raw --type=constant --allow-root
+
+# Plugins
+echo "Installing plugins"
+wp plugin install notification --force --activate --allow-root
+wp plugin install blackbar --force --activate --allow-root
+# Continue
+exec "apache2-foreground"
